d21e204902c89898b109f05d5e55bd53223e4a60
deathsy <ks.srikittichai@gmail.com>
2020-03-09 17:24:40 +0700

    🔥 adding pokemonData service

--- src/__tests__/09.js
+++ src/__tests__/09.js
@@ -1,7 +1,18 @@
-import promiseSum from "../final/09";
+import { promiseSum, getPokemonDataPromise } from "../exercises/09";
 
 describe("09", () => {
-  test("should ", () => {
+  test("should resolved fetch promise correctly", () => {
+    const data = { base_experience: 101, height: 3, weight: 40 };
+
+    expect.assertions(3);
+    getPokemonDataPromise().then(pokemonData => {
+      expect(pokemonData.base_experience).toBe(data.base_experience);
+      expect(pokemonData.height).toBe(data.height);
+      expect(pokemonData.weight).toBe(data.weight);
+    });
+  });
+
+  test("should resolved custom promise correctly", () => {
     expect(promiseSum(10, 10)).resolves.toBe(20);
   });
 });
