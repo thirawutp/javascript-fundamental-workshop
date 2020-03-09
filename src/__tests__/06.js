bfe73e8cd99f9da2886a346114592364c91cb8da
deathsy <ks.srikittichai@gmail.com>
2020-03-09 15:38:32 +0700

    📖 adding 6 doc

--- src/__tests__/06.js
+++ src/__tests__/06.js
@@ -1,12 +1,14 @@
 import { petTypeCount, letPetTalkByTypeAndName } from "../exercises/06";
 
-test("should count pet type correctly", () => {
-  const { cat, dog, bird } = petTypeCount();
-  expect(cat).toBe(2);
-});
+describe("06", () => {
+  test("should count pet type correctly", () => {
+    const { cat, dog, bird } = petTypeCount();
+    expect(cat).toBe(2);
+  });
 
-test("should count sound group by type correctly", () => {
-  expect(letPetTalkByTypeAndName("cat", "bengal")).toBe("meow");
-  expect(letPetTalkByTypeAndName("cat", "no-name")).toBeUndefined();
-  expect(letPetTalkByTypeAndName("dog", "fluffykins")).toBeUndefined();
+  test("should count sound group by type correctly", () => {
+    expect(letPetTalkByTypeAndName("cat", "bengal")).toBe("meow");
+    expect(letPetTalkByTypeAndName("cat", "no-name")).toBeUndefined();
+    expect(letPetTalkByTypeAndName("dog", "fluffykins")).toBeUndefined();
+  });
 });
